298e21cb14dea40bfb4d86720f2f3d1ec3da00af
Ruzanna <ruzanna30a@gmail.com>
2026-07-26 19:42:32 +0400

    "fix: remove items correctly during iteration"

--- oop_shoping_cart.py
+++ oop_shoping_cart.py
@@ -7,7 +7,7 @@ class ShoppingCart:
         return self._cart_list.copy()
 
     def remove_item(self, name):
-        for item in self._cart_list:
+        for item in self._cart_list.copy():
             if item[0] == name:
                 self._cart_list.remove(item)
         return self._cart_list.copy()
@@ -26,7 +26,10 @@ cart = ShoppingCart()
 print(cart.get_items())
 print(cart.add_item('banana', 20))
 cart.add_item('milk', 10)
+print(cart.add_item('banana', 10))
+print(cart.add_item('banana', 5))
 cart.add_item('potato', 2.5)
+print(cart.get_items())
 print(cart.total())
 cart.remove_item('banana')
 print(cart.total())
