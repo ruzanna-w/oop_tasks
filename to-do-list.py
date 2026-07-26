4020fcc8db6cbfbed5ea0d37985c99158eca4e08
Ruzanna <ruzanna30a@gmail.com>
2026-07-26 23:00:36 +0400

    feat: add is_overdue method to Task

--- to-do-list.py
+++ to-do-list.py
@@ -7,7 +7,7 @@ class Priority(Enum):
     Medium = 2
     Low = 3
 
-class Task:
+class Task():
     def __init__(self, title, deadline, description, priority=Priority.Unspecified, done=False):
         self.title = title
         self._deadline = deadline
@@ -31,6 +31,23 @@ class Task:
             return True
         else: False
 
+    def is_overdue(self):
+        if self._deadline < date.today() and self._done == False:
+            return True
 
-    
+task1 = Task("Купить продукты", date(2026, 7, 27), "Купить молоко, хлеб и овощи", Priority.High, False)
+task2 = Task("Выучить Python", date(2026, 7, 29), "Повторить Enum, циклы и функции", Priority.High, False)
+task3 = Task("Убраться дома", date(2026, 7, 25), "Пропылесосить и помыть пол", Priority.Medium, False)
+task4 = Task("Позвонить другу", date(2026, 7, 20), "Обсудить встречу на выходных", Priority.Low, False)
+task5 = Task("Сделать проект Todo List", date(2026, 8, 1), "Добавить классы Task и список задач", Priority.High, False)
+task6 = Task("Прочитать книгу", date(2026, 8, 5), "Прочитать 50 страниц", Priority.Medium, False)
+task7 = Task("Спорт", date(2026, 7, 31), "Сделать тренировку 30 минут", Priority.Low, False)
 
+tasks = [task1, task2, task3, task4, task5, task6, task7] 
+
+for task in tasks.copy():
+    if task.is_overdue() == True:
+        tasks.remove(task)
+
+for i in tasks:
+    print(i.title)
