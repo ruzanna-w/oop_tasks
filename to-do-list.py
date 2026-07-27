f8afc822220bf72044fda3ac6a271eba95111497
Ruzanna <ruzanna30a@gmail.com>
2026-07-27 12:44:57 +0400

    "feat: implement TaskManager class"

--- to-do-list.py
+++ to-do-list.py
@@ -7,7 +7,7 @@ class Priority(Enum):
     Medium = 2
     Low = 3
 
-class Task():
+class Task:
     def __init__(self, title, deadline, description, priority=Priority.Unspecified, done=False):
         self.title = title
         self._deadline = deadline
@@ -26,14 +26,31 @@ class Task():
         else:
             raise ValueError("Дата дедлайна не может быть в прошлом")
     
-    def missed_task(self):
-        if self._deadline < date.today() and self._done == False:
-            return True
-        else: False
-
     def is_overdue(self):
         if self._deadline < date.today() and self._done == False:
             return True
+        return False
+
+class TaskManager:
+    def __init__(self):
+        self._task_list = []
+
+    def add_task(self, task):
+        self._task_list.append(task)
+    
+    def get_all_tasks(self):
+        return self._task_list.copy()
+    
+    def get_overdue_tasks(self):
+        overdue_tasks = []
+        for task in self._task_list.copy():
+            if task.is_overdue():
+                overdue_tasks.append(task)
+        return overdue_tasks
+    
+    def remove_task(self, task):
+        if task in self._task_list.copy():
+            self._task_list.remove(task)
 
 task1 = Task("Купить продукты", date(2026, 7, 27), "Купить молоко, хлеб и овощи", Priority.High, False)
 task2 = Task("Выучить Python", date(2026, 7, 29), "Повторить Enum, циклы и функции", Priority.High, False)
@@ -43,11 +60,20 @@ task5 = Task("Сделать проект Todo List", date(2026, 8, 1), "Доб�
 task6 = Task("Прочитать книгу", date(2026, 8, 5), "Прочитать 50 страниц", Priority.Medium, False)
 task7 = Task("Спорт", date(2026, 7, 31), "Сделать тренировку 30 минут", Priority.Low, False)
 
+task_manager = TaskManager()
+
 tasks = [task1, task2, task3, task4, task5, task6, task7] 
 
-for task in tasks.copy():
-    if task.is_overdue() == True:
-        tasks.remove(task)
+for task in tasks:
+    task_manager.add_task(task)
 
-for i in tasks:
-    print(i.title)
+for task in task_manager.get_all_tasks():
+    print(task.title)
+
+for task in task_manager.get_overdue_tasks():
+    print(task.title, task.deadline)
+
+task_manager.remove_task(task1)
+
+for task in task_manager.get_all_tasks():
+    print(task.title)
